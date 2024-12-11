05da3034395fee9a8421851b58b13f53ffcfed20
TIshow <febooooooo@gmail.com>
2024-12-11 18:56:11 +0900

    Google Mapとの連携。ログイン後画面の実装

---
+++ ios/Runner/Env.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct Env {
+  static let googleMapApiKey = "AIzaSyBYRPwD04kYJ7LY_-QN681JkGZHjkAxhrw"
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -8,6 +10,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.ios,
   );
+  await dotenv.load(fileName: ".env"); // Load the .env
   runApp(const MyApp());
 }
 
@@ -22,7 +25,28 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
       ),
-      home: const LoginPage(),
+      home: const AuthWrapper(),
+    );
+  }
+}
+
+// A wrapper to handle redirection based on login state
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasData) {
+          return const HomePage(); // Navigate to HomePage if logged in
+        } else {
+          return const LoginPage(); // Show LoginPage if not logged in
+        }
+      },
     );
   }
 }
@@ -36,39 +60,12 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  User? user;
-
-  @override
-  void initState() {
-    super.initState();
-    user = _auth.currentUser;
-  }
 
   Future<void> anonymousSignIn() async {
     try {
-      UserCredential userCredential = await _auth.signInAnonymously();
-      setState(() {
-        user = userCredential.user;
-      });
+      await _auth.signInAnonymously();
     } catch (e) {
       print("Failed to sign in anonymously: $e");
-    }
-  }
-
-  Future<void> linkWithEmailAndPassword(String email, String password) async {
-    try {
-      if (user != null && user!.isAnonymous) {
-        AuthCredential credential =
-            EmailAuthProvider.credential(email: email, password: password);
-        UserCredential userCredential =
-            await user!.linkWithCredential(credential);
-        setState(() {
-          user = userCredential.user;
-        });
-        print("Anonymous account linked with email and password.");
-      }
-    } catch (e) {
-      print("Failed to link anonymous account: $e");
     }
   }
 
@@ -79,31 +76,102 @@ class _LoginPageState extends State<LoginPage> {
         title: const Text('Anonymous Login'),
       ),
       body: Center(
-        child: user == null
-            ? ElevatedButton(
-                onPressed: anonymousSignIn,
-                child: const Text('Sign in Anonymously'),
-              )
-            : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(user!.isAnonymous
-                      ? 'Signed in anonymously'
-                      : 'Signed in as ${user!.email}'),
-                  const SizedBox(height: 20),
-                  if (user!.isAnonymous) ...[
-                    ElevatedButton(
-                      onPressed: () async {
-                        // Replace with actual email and password inputs
-                        String email = "test@example.com";
-                        String password = "password123";
-                        await linkWithEmailAndPassword(email, password);
-                      },
-                      child: const Text('Link with Email/Password'),
+        child: ElevatedButton(
+          onPressed: anonymousSignIn,
+          child: const Text('Sign in Anonymously'),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late GoogleMapController mapController;
+
+  // Check Google Maps has loaded properly
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    print("Google Map has been loaded successfully.");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(35.6895, 139.6917), // Tokyo coordinates
+              zoom: 15,
+            ),
+            onMapCreated: _onMapCreated,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
                     ),
-                  ],
-                ],
-              ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        spreadRadius: 2,
+                        blurRadius: 10,
+                      ),
+                    ],
+                  ),
+                  child: const Center(
+                    child: Text('Tab Area'),
+                  ),
+                ),
+                Container(
+                  color: const Color(0xFFE6E0E9), // Background color
+                  child: BottomNavigationBar(
+                    backgroundColor: const Color(0xFFE6E0E9),
+                    type: BottomNavigationBarType.fixed,
+                    showSelectedLabels: false,
+                    showUnselectedLabels: false,
+                    items: const [
+                      BottomNavigationBarItem(
+                        icon:
+                            Icon(Icons.location_pin, color: Color(0xFF1D1B20)),
+                        label: '',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.add_circle, color: Color(0xFF1D1B20)),
+                        label: '',
+                      ),
+                      BottomNavigationBarItem(
+                        icon:
+                            Icon(Icons.notifications, color: Color(0xFF1D1B20)),
+                        label: '',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.account_circle,
+                            color: Color(0xFF1D1B20)),
+                        label: '',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
