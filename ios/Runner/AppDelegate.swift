7a3068d214ab5f1bc95c11aac9f4a5ee0983f24a
TIshow <febooooooo@gmail.com>
2024-12-30 17:32:09 +0900

    【Bug】Google Mapが表示されないバグの修正

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey(Env.googleMapApiKey)
+    GMSServices.provideAPIKey("AIzaSyDrnjbtAc-5XSRxf1QtZ-oN0KzoXXQuhO8")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
