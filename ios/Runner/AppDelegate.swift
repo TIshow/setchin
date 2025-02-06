305497eefd5fccb5414c2f3febee267c4f4a36d4
coiai <coiaichan@gmail.com>
2025-02-06 23:30:16 +0900

    Set Google Maps API key using Xcode environment variables in AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,14 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey(ProcessInfo.processInfo.environment["AIzaSyDrnjbtAc-5XSRxf1QtZ-oN0KzoXXQuhO8"]!)
+    
+      // Xcode の環境変数から APIキー を取得
+      if let apiKey = ProcessInfo.processInfo.environment["GOOGLE_MAPS_API_KEY"], !apiKey.isEmpty {
+          GMSServices.provideAPIKey(apiKey)
+      } else {
+          fatalError("Google Maps API Key is missing. Please set GOOGLE_MAPS_API_KEY in Xcode's Environment Variables.")
+      }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
