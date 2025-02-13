f0ae38821e553a41d81d938bb67d0edf46e9fb1a
TIshow <febooooooo@gmail.com>
2025-02-13 16:48:53 +0900

    fix: load API and firebase config properly

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,12 +9,8 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     
-      // Xcode の環境変数から APIキー を取得
-      if let apiKey = ProcessInfo.processInfo.environment["GOOGLE_MAPS_API_KEY"], !apiKey.isEmpty {
-          GMSServices.provideAPIKey(apiKey)
-      } else {
-          fatalError("Google Maps API Key is missing. Please set GOOGLE_MAPS_API_KEY in Xcode's Environment Variables.")
-      }
+    // Xcode の環境変数から APIキー を取得
+    GMSServices.provideAPIKey(Env.googleMapApiKey)
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
